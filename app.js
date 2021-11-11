c98409f064d9da640143985e08c7ae6aacf06fce
david little <david.little@gmail.com>
2021-11-10 19:30:44 -0600

    trying to merge both repos

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -7,21 +6,10 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-=======
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
->>>>>>> 07aa54c52240166f5891cc653b1fc9ce93606702
 
 var app = express();
 
 // view engine setup
-<<<<<<< HEAD
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
@@ -36,27 +24,10 @@ app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-=======
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
->>>>>>> 07aa54c52240166f5891cc653b1fc9ce93606702
   next(createError(404));
 });
 
 // error handler
-<<<<<<< HEAD
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -65,16 +36,6 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
-=======
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
->>>>>>> 07aa54c52240166f5891cc653b1fc9ce93606702
 });
 
 module.exports = app;
